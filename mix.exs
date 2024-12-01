c7732e17f6c4e2168ddce4d4ca219eadc77da1f6
E.FU <e-fu@users.noreply.github.com>
2024-12-01 12:41:49 +0800

    prepare for hex.pm

--- mix.exs
+++ mix.exs
@@ -2,17 +2,18 @@ defmodule MetaHeexComponent.MixProject do
   use Mix.Project
 
   @version "0.1.0"
-  @source_url "https://github.com/yourusername/meta_heex_component"
+  @source_url "https://github.com/e-fu/meta_heex_component.git"
 
   def project do
     [
       app: :meta_heex_component,
       version: @version,
       elixir: "~> 1.14",
+      phoenix: "~> 1.7.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
-      description: "Dynamic meta tag management for Phoenix LiveView applications"
+      description: "Dynamic meta tag management for Phoenix LiveView and Controller applications"
     ]
   end
 
@@ -33,10 +34,15 @@ defmodule MetaHeexComponent.MixProject do
 
   defp package do
     [
-      maintainers: ["Your Name"],
+      maintainers: ["e.fu"],
       licenses: ["MIT"],
       links: %{"GitHub" => @source_url},
-      files: ~w(lib LICENSE.md mix.exs README.md)
+      files: ~w(lib LICENSE.md mix.exs README.md),
+      build_tools: ["mix"],
+      extra: %{
+        "type" => "library",
+        "documentation" => "https://hexdocs.pm/meta_heex_component"
+      }
     ]
   end
 end
